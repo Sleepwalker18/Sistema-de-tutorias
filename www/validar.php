e4e7e4dc29043ca9edd58eca9c2cf01dc9528093
Lololala09 <47404804+Lololala09@users.noreply.github.com>
2020-12-08 20:37:04 -0600

    Login
    El login custom y la validacion

---
+++ www/validar.php
@@ -0,0 +1,30 @@
+<?php
+include('db.php');
+$usuario=$_POST['id'];
+$contraseña=$_POST['password'];
+session_start();
+$_SESSION['id']=$usuario;
+
+
+$conexion=mysqli_connect("localhost","root","","dbname");
+
+$consulta="SELECT*FROM usuario where id=\'$usuario\' and password=\'$contraseña\'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+
+    header("location:index.html");
+
+}else{
+    ?>
+    <?php
+    include("login.html");
+
+  ?>
+  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+  <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
