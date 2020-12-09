61c409f4ef27bad12e848b43a255b3401d06b30b
sleepwalker18 <isradarrellgdz@gmail.com>
2020-12-08 21:13:29 -0600

    update db

--- final.sql
+++ final.sql
@@ -1,10 +1,14 @@
-CREATE TABLE usuario (
-	id integer NOT NULL AUTO_INCREMENT,
+CREATE DATABASE dbname;
+USE dbname;
+CREATE TABLE usuarios (
+	id_usuario integer NOT NULL AUTO_INCREMENT,
+	nombre varchar(50) NOT NULL,
+	usuario varchar(50) NOT NULL,
 	password varchar(16) NOT NULL,
-	PRIMARY KEY(id)
+	PRIMARY KEY(id_usuario)
 );
 
-CREATE TABLE alumno(
+CREATE TABLE alumnos(
 	matricula varchar(6) NOT NULL,
 	nombre varchar(60) NOT NULL,
 	edad integer NOT NULL,
@@ -14,18 +18,18 @@ CREATE TABLE alumno(
 	PRIMARY KEY(matricula)
 );
 
-CREATE TABLE personal (
-	id_personal integer NOT NULL AUTO_INCREMENT,
+CREATE TABLE informacion_personal (
+	id_informacion_personal integer NOT NULL AUTO_INCREMENT,
 	matricula varchar(6) NOT NULL,
 	proyecto_personal varchar(50),
 	fortaleza1 varchar(50),
 	fortaleza2 varchar(50),
 	fortaleza3 varchar(50),
-	devilidad1 varchar(50),
-	devilidad2 varchar(50),
-	devilidad3 varchar(50),
+	debilidad1 varchar(50),
+	debilidad2 varchar(50),
+	debilidad3 varchar(50),
 	decidir_proyecto boolean,
 	describir_proyecto varchar(100),
-	PRIMARY KEY(id_personal),
-    FOREIGN KEY (matricula) REFERENCES alumno(matricula)
+	PRIMARY KEY(id_informacion_personal),
+    FOREIGN KEY (matricula) REFERENCES alumnos(matricula)
 );
